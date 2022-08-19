fd0b141fb7e35e6c99ab6e6c4b2ad25b262abe8a
Simon Strassl <simon@sigmoid.at>
2022-08-19 19:57:11 +0200

    fix hledger decoding error

--- docker_run.sh
+++ docker_run.sh
@@ -5,4 +5,4 @@ IFS=$'\n\t'
 CODE_DIR="$(pwd)/src"
 
 DOCKER_IMG_HASH="$(docker build -q ./docker)"
-docker run -it --rm -v "$CODE_DIR:/code" -v "$DEST_DIR:/dest" "$DOCKER_IMG_HASH" "$@"
+docker run -it --rm -v "$CODE_DIR:/code" -v "$DEST_DIR:/dest" -e LANG=C.UTF-8 "$DOCKER_IMG_HASH" "$@"
