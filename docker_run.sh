20ab8f355ece73c69729d3d27e5ed3a3f13a7c5f
Simon Strassl <simon@sigmoid.at>
2022-08-21 20:12:57 +0200

    only bind to port 5000 on host if hledger web is being run

--- docker_run.sh
+++ docker_run.sh
@@ -4,5 +4,5 @@ IFS=$'\n\t'
 
 CODE_DIR="$(pwd)/src"
 
-DOCKER_IMG_HASH="$(docker build -q ./docker)"
-docker run -it --rm -v "$CODE_DIR:/code" -v "$DEST_DIR:/dest" -e LANG=C.UTF-8 -e LEDGER_FILE="/dest/main.journal" -p "127.0.0.1:5000:5000" "$DOCKER_IMG_HASH" "$@"
+DOCKER_IMG_HASH="$(./docker_build.sh)"
+docker run -it --rm -v "$CODE_DIR:/code" -v "$DEST_DIR:/dest" -e LANG=C.UTF-8 -e LEDGER_FILE="/dest/main.journal" "${DOCKER_ARGS[@]}" "$DOCKER_IMG_HASH" "$@"
