d61b4288f796673b28cf3f09b27bcbb0a37360e8
Simon Strassl <simon@sigmoid.at>
2022-08-21 18:59:39 +0200

    fix hledger web URLs referring to 0.0.0.0

--- run_hledger_web.sh
+++ run_hledger_web.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-DEST_DIR="$1" ./docker_run.sh hledger web -- --serve --host="0.0.0.0"
+DEST_DIR="$1" ./docker_run.sh hledger web -- --serve --host="0.0.0.0" --base-url="http://127.0.0.1:5000"
