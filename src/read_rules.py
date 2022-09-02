703680b05cef8e8207b97cdfdc120ecc5e1dd705
Simon Strassl <simon@sigmoid.at>
2022-09-02 20:56:14 +0200

    remove leftover, commented-out code

--- src/read_rules.py
+++ src/read_rules.py
@@ -11,33 +11,3 @@ def read_rules(rules_file_path):
     loader.exec_module(module)
 
     return module.make_converter()
-
-
-#def parse_rules(base_path, format_args):
-#    rules = configparser.ConfigParser(delimiters=("=",))
-#    rules.read(os.path.join(base_path, "rules.ini"), encoding="UTF-8")
-#
-#    result = []
-#    for rule, account in rules["DEFAULT"].items():
-#        parts = rule.split("$")
-#        if parts[0] == "text":
-#            result.append((lambda entry, regex=re.compile(parts[1], re.I | re.M | re.DOTALL): bool(regex.search(entry.text)), account))
-#        else:
-#            raise Exception("Unknown rule syntax " + rule)
-#
-#    def fallback(entry):
-#        print(f"Warning: Had to apply fallback rule to entry: {utils.namedtuple_pformat(entry, format_args)}")
-#        return True
-#    result.append((fallback, "Unbekannt"))
-#
-#    return result
-
-#                    # The rule could be a compound rule (using the + operator)
-#                    destination = []
-#                    for component in rule[1].split("+"):
-#                        component = component.strip()
-#                        parts = component.split("=")
-#                        if len(parts) == 1:
-#                            destination.append((parts[0].strip(), None, None))
-#                        else:
-#                            destination.append((parts[0].strip(), utils.parse_num_str(parts[1], format_args.decimal_separator), entry.currency))
