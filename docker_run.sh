f46546d77a9625ba528703c27fd63db1dcb4cc0d
Simon Strassl <simon@sigmoid.at>
2023-02-05 15:28:08 +0100

    run docker as current user
    fixes files being written to mounted volume as root

--- docker_run.sh
+++ docker_run.sh
@@ -5,4 +5,4 @@ IFS=$'\n\t'
 CODE_DIR="$(pwd)/src"
 
 DOCKER_IMG_HASH="$(./docker_build.sh)"
-docker run -it --rm -v "$CODE_DIR:/code" -v "$DEST_DIR:/dest" -e LANG=C.UTF-8 -e LEDGER_FILE="/dest/main.journal" "${DOCKER_ARGS[@]}" "$DOCKER_IMG_HASH" "$@"
+docker run -it --rm -u "$(id -u $USER)" -v "$CODE_DIR:/code" -v "$DEST_DIR:/dest" -e LANG=C.UTF-8 -e LEDGER_FILE="/dest/main.journal" "${DOCKER_ARGS[@]}" "$DOCKER_IMG_HASH" "$@"
