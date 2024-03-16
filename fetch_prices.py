fcb038dd852575d4e8891488c45661bcc51f8411
Simon Strassl <simon@sigmoid.at>
2024-03-16 12:29:14 +0100

    properly quote non-alpha commodity symbols in prices

--- fetch_prices.py
+++ fetch_prices.py
@@ -1,6 +1,6 @@
 import requests
 
-from .utils import format_number_exact, parse_num_us
+from .utils import format_number_exact, parse_num_us, escape_commodity
 
 QUERY_URL = "https://www.alphavantage.co/query"
 
@@ -22,7 +22,7 @@ def fetch(api_key, load, format_args):
 			result = result.json()
 			for date, values in result["Time Series (Daily)"].items():
 				price = parse_num_us(values['4. close'])
-				output += f"P {date} {key} {format_number_exact(price, format_args, min_decimal=4)} {currency}\n"
+				output += f"P {date} {escape_commodity(key)} {format_number_exact(price, format_args, min_decimal=4)} {escape_commodity(currency)}\n"
 		elif type == 'FX':
 			from_symbol = value['from_symbol']
 			to_symbol = value['to_symbol']
@@ -36,7 +36,7 @@ def fetch(api_key, load, format_args):
 			result = result.json()
 			for date, values in result["Time Series FX (Daily)"].items():
 				price = parse_num_us(values['4. close'])
-				output += f"P {date} {from_symbol} {format_number_exact(price, format_args, min_decimal=4)} {to_symbol}\n"
+				output += f"P {date} {escape_commodity(from_symbol)} {format_number_exact(price, format_args, min_decimal=4)} {escape_commodity(to_symbol)}\n"
 		else:
 			assert False, "Unknown type"
 	return output
